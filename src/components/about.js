69bb75255e72818edccceec2681f66bffb28100a
deannapi <deannapi.mart@gmail.com>
2021-04-28 10:23:54 -0500

    added pricing

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,71 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class About extends React.Component {
   render() {
-    return <>{/* how to join */}</>;
+    return (
+      <>
+        <div className="price">
+          <h3>Price</h3>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="card">
+                <div className="card-header">Unlimited</div>
+                <div className="card-body">
+                  <h6 className="card-subtitle mb-2 ">$150</h6>
+                  <p className="card-text">
+                    Month To Month <br /> Unlimited access to classes
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header">Discounted</div>
+                <div className="card-body">
+                  <h6 className="card-subtitle mb-2 ">$120</h6>
+                  <p className="card-text">
+                    Month To Month <br /> Unlimited access to classes
+                    <br />
+                    <li>Students</li>
+                    <li>Teachers</li>
+                    <li>Emergency Personnel</li>
+                    <li>Military</li>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row align-items-center">
+              <div className="card">
+                <div className="card-header">Couples</div>
+                <div className="card-body">
+                  <h6 className="card-subtitle mb-2 ">$240</h6>
+                  <p className="card-text">
+                    Month To Month <br /> Unlimited access to classes <br />{" "}
+                    <li>Couples</li>
+                  </p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card-header">Family</div>
+                <div className="card-body">
+                  <h6 className="card-subtitle mb-2 ">$300</h6>
+                  <p className="card-text">
+                    Month To Month <br /> Unlimited access to classes
+                    <br />
+                    <li>Family of 3</li>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* end of cards */}
+        <div>
+          <h3>
+            <Link to="/">Get Started</Link>
+          </h3>
+        </div>
+      </>
+    );
   }
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -6,38 +6,59 @@ export default class Footer extends React.Component {
     return (
       <>
         <footer>
-            <div className="copyright">
-                <h5>&copy; 2012 Crossfit 432 - All Rights Reserved</h5>
-            </div>
-          <ul>
-            <li className="instagram">
-              <a
-                href="https://www.instagram.com/crossfit432/"
-                target="_blank"
-                rel="noopener noreferrer"
+          <div className="copyright">
+            <h5>&copy; 2021 Crossfit 432 - All Rights Reserved</h5>
+            <h5>Crossfit 432 Est. 2012</h5>
+          </div>
+          <div className="phone">
+            <a href="tel:14327701195">
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                className="bi bi-telephone-fill"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <i class="fab fa-instagram"></i>
-              </a>
-            </li>
-            <li className="facebook">
-              <a
-                href="https://www.facebook.com/Crossfit432/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i class="fab fa-facebook-f"></i>
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.chalkculture.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={chalk_culture} alt="" id="chalk_cult" />
-              </a>
-            </li>
-          </ul>
+                <path
+                  fillRule="evenodd"
+                  d="M2.267.98a1.636 1.636 0 0 1 2.448.152l1.681 2.162c.309.396.418.913.296 1.4l-.513 2.053a.636.636 0 0 0 .167.604L8.65 9.654a.636.636 0 0 0 .604.167l2.052-.513a1.636 1.636 0 0 1 1.401.296l2.162 1.681c.777.604.849 1.753.153 2.448l-.97.97c-.693.693-1.73.998-2.697.658a17.47 17.47 0 0 1-6.571-4.144A17.47 17.47 0 0 1 .639 4.646c-.34-.967-.035-2.004.658-2.698l.97-.969z"
+                />
+              </svg>
+              432.770.1192
+            </a>
+          </div>
+          <div>
+            <ul>
+              <li className="instagram">
+                <a
+                  href="https://www.instagram.com/crossfit432/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="fab fa-instagram"></i>
+                </a>
+              </li>
+              <li className="facebook">
+                <a
+                  href="https://www.facebook.com/Crossfit432/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.chalkculture.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={chalk_culture} alt="" id="chalk_cult" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </footer>
       </>
     );
